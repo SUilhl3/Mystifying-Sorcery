da270a1ad1158fbb0c047a561f9610c495a1fd3f
Tajinder <tnijjar2@my.centennialcollege.ca>
2025-11-07 20:10:20 -0500

    Make simple first level
    Extended the level and added a camera follow script to make a very simple and small level. Also fixed some issues with the obstacle prefabs.

---
+++ Assets/_Project/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float height = 2;
+    public float depth = -10;
+
+    //follows the player as they move around horizontally (x-axis) 
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(
+            target.position.x,
+            height,
+            depth
+            );
+    }
+}
