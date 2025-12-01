5c3582575bb61ca0b1e3d980c14bb5fdefdc771b
Tajinder <tnijjar2@my.centennialcollege.ca>
2025-11-30 20:26:39 -0500

    Added some obstacles to level 2
    Built out level 2 a little more adding obstacles, you can finish the whole level going from the bottom to the top. Keys can also be collected now.

--- Assets/_Project/Scripts/Obstacles/ProjectileBlock.cs
+++ Assets/_Project/Scripts/Obstacles/ProjectileBlock.cs
@@ -24,7 +24,7 @@ public class ProjectileBlock : MonoBehaviour
         } else
         {
             detectionDirection = Vector2.right;
-            transform.Rotate(0f, 180f, 0f);
+            //transform.Rotate(0f, 180f, 0f);
         }
     }
 
