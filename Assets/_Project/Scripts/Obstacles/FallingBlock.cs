4d0ed345692973a773fb40c61f589bf4a287c5b2
Tajinder <tnijjar2@my.centennialcollege.ca>
2025-12-02 21:25:03 -0500

    Update player and levels
    Made the player speed slower, and tried to make the levels a little easier based on the feedback reports. I also updated the camera and the fallingblock prefab to work a little better.

--- Assets/_Project/Scripts/Obstacles/FallingBlock.cs
+++ Assets/_Project/Scripts/Obstacles/FallingBlock.cs
@@ -8,6 +8,7 @@ public class FallingBlock : MonoBehaviour
     [SerializeField] float fallSpeed;
     [SerializeField] LayerMask playerLayer;
     private bool isFalling = false;
+    private bool grounded = false;
     public int damage = 10;
     Rigidbody2D rb;
 
@@ -41,10 +42,17 @@ public class FallingBlock : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.collider.tag == "Player")
+        // so only bottom part of block kills player, they can safely jump on the top part
+        if (collision.collider.tag == "Player" && collision.otherCollider.tag =="TempTag" && !grounded)
         {
             PlayerController pc = collision.collider.GetComponent<PlayerController>();
             pc.TakeDamage(damage);
+
+            //disable collider once it hits the ground so the player wont die by touching it
+        } else if (collision.collider.tag != "Player" && collision.otherCollider.tag == "TempTag")
+        {
+            grounded = true;
         }
     }
+
 }
